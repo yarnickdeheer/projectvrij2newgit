7d20fda7ab4813b81398b9368b8131745217b66e
Smos Bois <simondeb1505@gmail.com>
2021-05-29 20:26:14 +0200

    turtle kan route volgen nu

---
+++ Assets/simon/scripts/CleanNoteCodes/TurtleRoute.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleRoute : MonoBehaviour
+{
+    public Transform[] route;
+
+    public Vector3 pointOne;
+    public Vector3 pointTwo;
+    public Vector3 curvePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rechtdoor is:
+        float distanceBetweenOneAndTwo = Vector3.Distance(pointOne, pointTwo);
+        float moveSpeed = 0.1f;
+
+        transform.position = transform.position + (pointTwo - pointOne).normalized * moveSpeed;
+        if(Vector3.Distance(transform.position, pointTwo) < 2)
+        {
+            transform.position = pointTwo;
+        }
+    }
+}
