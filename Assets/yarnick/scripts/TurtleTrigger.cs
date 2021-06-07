e8f633705b9d93e3b6811a971e9a7d296085c2b2
Smos Bois <simondeb1505@gmail.com>
2021-06-07 20:28:29 +0200

    Revert "FMOD Water"
    This reverts commit 398307a60373be83d0cc462dac9d1c0bfcc4e2c4, reversing
    changes made to 314a874369aed5944cb7106c12b0b4f2a246a5ae.

--- Assets/yarnick/scripts/TurtleTrigger.cs
+++ Assets/yarnick/scripts/TurtleTrigger.cs
@@ -14,7 +14,6 @@ public class TurtleTrigger : MonoBehaviour
     public float speed =2;
     public GameObject player;
     private Vector3 pos;
-    public bool mounted;
 
     // Start is called before the first frame update
     void Start()
@@ -69,15 +68,11 @@ public class TurtleTrigger : MonoBehaviour
             player.GetComponent<Animator>().SetBool("frontmovement", false);
             player.GetComponent<Animator>().SetBool("backmovement", false);
             player.GetComponent<Animator>().SetBool("sidemovement", false);
-            player.GetComponent<Animator>().SetBool("backjump", false);
-            player.GetComponent<Animator>().SetBool("sidejump", false);
-            player.GetComponent<Animator>().SetBool("viool", true);
             //player.GetComponent<Animator>().SetBool("viool", false);
             player.transform.parent = this.transform.parent;
             player.transform.position = pos;
             player.transform.eulerAngles = new Vector3(0,0,0);
             tut = true;
-            mounted = true;
         }
             if (other.gameObject.tag == "end" && ride == false)
         {
@@ -99,7 +94,6 @@ public class TurtleTrigger : MonoBehaviour
             player.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
             player.transform.parent = oldParent; 
             tut = false;
-            mounted = false;
         }
     }
 
