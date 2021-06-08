6ff2a402b8fff2c22decf08cebefa2f78b85883e
Tohunity71 <t.huynh718@gmail.com>
2021-06-08 16:12:04 +0200

    Added UI test

---
+++ Assets/Tommy/UI assets/CallPauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallPauseMenu : MonoBehaviour
+{
+    public bool pauseIsOn;
+    public GameObject PauseMenu;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        PauseMenu.SetActive(pauseIsOn);
+
+        if (Input.GetKeyDown(KeyCode.Escape) && !pauseIsOn)
+        {
+            Time.timeScale = 0;
+            pauseIsOn = true;
+        } 
+        else if (Input.GetKeyDown(KeyCode.Escape) && pauseIsOn)
+        {
+            Time.timeScale = 1;
+            pauseIsOn = false;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseIsOn = false;
+    }
+}
