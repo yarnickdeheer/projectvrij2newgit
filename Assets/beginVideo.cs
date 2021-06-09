a8e88d452001aed9735e9a33187585c2387916d0
Smos Bois <simondeb1505@gmail.com>
2021-06-09 05:43:33 +0200

    final stretch

---
+++ Assets/beginVideo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class beginVideo : MonoBehaviour
+{
+    private void Update()
+    {
+        VideoPlayer player = GetComponent<VideoPlayer>();
+        player.loopPointReached += finishgame;
+    }
+    void Endvideo()
+    {
+        Debug.Log("EINDIG DIE VIDEO");
+        SceneManager.LoadScene("playtestv2");
+    }
+
+    void finishgame(VideoPlayer player)
+    {
+        // bf.SetActive(true);
+        /// Screen.lockCursor = true;
+        /// Cursor.visible = false;
+        //controller.enabled = true;
+
+        Endvideo();
+        // image.enabled = false;
+        Debug.Log("done");
+
+        //  Time.timeScale = 1;
+    }
+}
---
+++ Assets/endTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class endTrigger : MonoBehaviour
+{
+    private void Update()
+    {
+        VideoPlayer player = GetComponent<VideoPlayer>();
+        player.loopPointReached += finishgame;
+    }
+    void Endvideo()
+    {
+        Debug.Log("EINDIG DIE VIDEO");
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    void finishgame(VideoPlayer player)
+    {
+        // bf.SetActive(true);
+        /// Screen.lockCursor = true;
+        /// Cursor.visible = false;
+        //controller.enabled = true;
+
+        Endvideo();
+        // image.enabled = false;
+        Debug.Log("done");
+
+        //  Time.timeScale = 1;
+    }
+}
